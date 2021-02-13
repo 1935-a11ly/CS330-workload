d9e6fd4220ef6434b86edf79b47bb08113492051
Michael <musami01@luther.edu>
2021-02-13 11:47:59 -0600

    exercise_1

--- exercise_1/primes.js
+++ exercise_1/primes.js
@@ -1,29 +1,76 @@
+/* Michael Musa */
+/* Primes Exercise */
+
+
 /* jshint esversion: 8 */
 /* jshint node: true */
 'use strict';
+
 
 var urlParams = new URLSearchParams(window.location.search);
 
 function greet() {
     const queryString = window.location.search;
+
+
+    /* Using queryString from lecture videos
+    */
     const urlParams = new URLSearchParams(queryString);
-    const name_Info=urlParams.get('name')||'Student';
+
+
+    /* Gets name form URL. Defaults to Student if the user did not input any
+    */
+    const name_Information=urlParams.get('name')||'Student';
+
+   
+    /* Selects h1 from html
+    */
     let welcome_Greeting = document.querySelector("h1");
-    welcome_Greeting.innerText = `Hello ${name_Info}`;
+
+
+    /* Changes h1 to greeting based on the name input by user
+    */
+    welcome_Greeting.innerText = `Hello ${name_Information}`;
+   
 }
 
 function isPrime(n) {
+
+
+     /* z is always smaller than the parameter
+    */
+    /* And this condition will not allow the parameter to be divisible by itself
+    */
     for (var z=2; z<n; z++) 
+
+
+    /* When the parameter is divisible by z it's not a prime
+    */
         if ( n % z === 0) {
             return false;
         }
+    
+
+    /* Parameter must be greater than 1 since 1 is not Prime
+    */ 
     return n>1;
+
 }
 
 function printPrimeNumber() {
     const queryString=window.location.search;
+
+
+     /* Using queryString from lecture videos
+    */ 
     const urlParams=new URLSearchParams(queryString);
+
+
+    /* Gets n input by user in URL. if there is none defaults to 330
+    */
     const n=urlParams.get('n')||330;
+
+
     if(isPrime(n)){
         document.querySelector("#primeInfo").innerText=`${n} is a prime number`;
     }
@@ -34,15 +81,18 @@ function printPrimeNumber() {
 
 
 function getNPrimes(n) {
-    var list_primes = [];
+
+    var listOfPrimes = [];
+
     var y=2;
+
     for (var z=0; z<n; z++){
-        while(list_primes.length<n){
-            if(isPrime(y)) list_primes.push(y);
+        while(listOfPrimes.length<n){
+            if(isPrime(y)) listOfPrimes.push(y);
             y++;
         }
     }
-    return list_primes
+    return listOfPrimes
 }
 
 
@@ -50,12 +100,14 @@ function printNPrimes() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const n=urlParams.get('n')||330;
-    let thead= document.querySelector("thead");
-    thead.innerHTML= `First ${n} primes`;
+
+    let table_Header= document.querySelector("thead");
+    table_Header.innerHTML= `First ${n} primes`;
+
         for (var x of getNPrimes(n)) {
-            var tableData=document.querySelector("tbody");
-            var nextRow=tableData.insertRow();
-            nextRow.innerText=`${x}`;
+            var table_Information=document.querySelector("tbody");
+            var next_Row=table_Information.insertRow();
+            next_Row.innerText=`${x}`;
             }
 
 }
