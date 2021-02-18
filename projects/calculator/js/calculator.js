b246d9f7773456a20f4cfde878c9f6ae76b03c63
Michael <musami01@luther.edu>
2021-02-18 13:43:24 -0600

    AnotherCommit

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -6,19 +6,24 @@
 var screen;
 
 
-function enterDigit(digit) {
+function enterDigit(val) {
+    document.querySelector("#result").innerText=`${val}`;
 }
 
+
 function clear_screen() {
+
 }
 
 function eval_expr() {
 }
 
 function enterOp(operation) {
+    document.querySelector("#result").innerText=`${operation}`;
 }
 
 window.onload = function () {
     screen = document.querySelector("#result");
     screen.innerHTML = "0";
+    
 };
