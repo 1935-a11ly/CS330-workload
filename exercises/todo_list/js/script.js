6ff95acbda919bf62e65dcc79b72c1d415b9669b
Michael <musami01@luther.edu>
2021-02-27 11:55:15 -0600

    AnotherCommit

--- exercises/todo_list/js/script.js
+++ exercises/todo_list/js/script.js
@@ -4,40 +4,101 @@
 
 var team = ["Aardvark", "Beaver", "Cheetah", "Dolphin", "Elephant", "Flamingo", "Giraffe", "Hippo"];
 var priority = ["Low", "Normal", "Important", "Critical"];
+let td = document.createElement("td");
+
+class Task {
+    constructor(task, team, priority,dueDate) {
+        this._task  = task;
+        this._team = team;
+        this._priority = priority;
+        this._dueDate = dueDate;
+    }
+
+    get task() {
+        return this._task;
+    }
+
+    get team() {
+        return this._team;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    get dueDate(){
+        return this._dueDate;
+    }
+    
+}
+
+
 
 function addTask() {
+    let vals = [];
+    let rowcolids = ["title", "assignedTo", "priority", "dueDate"];
+
     if (!document.querySelector("#newTask").checkValidity()) {
         let inputWarning = document.createElement("p");
         inputWarning.setAttribute("class", "alert alert-warning");
-        inputWarning.innerText = "All options must be filled";
+        inputWarning.innerText = "Fill out title and due date";
         document.querySelector("body").appendChild(inputWarning);
         return;
     }
     else{
 
-        let vals = [];
-        let rowcolids = ["title", "assignedTo", "priority", "dueDate"];
-    
-        addRow(vals, document.getElementById("taskList"));
+    let titleText = document.querySelector("#title").value;
+    vals.push(titleText);
+    let teamOptions = document.querySelector("#assignedTo").selectedOptions[0].value;
+    vals.push(teamOptions);
+    let priorityOptions = document.querySelector("#priority").selectedOptions[0].value;
+    vals.push(priorityOptions);
+    let dueDateOptions = document.querySelector("#dueDate").value;
+    vals.push(dueDateOptions);
 
-    }
-
+    addRow(vals, document.getElementById("taskList"));
+}
 }
 
+
 function addRow(valueList, parent) {
-    let row = document.createElement("tr");
     let td = document.createElement("td");
+    let row = document.createElement("tr");
     let cb = document.createElement("input");
-
-
+    cb.setAttribute("type", "checkbox");
+    cb.setAttribute("id", "eliminate");
+    cb.setAttribute("onclick", "removeRow();");
+    
+    let checker = document.createElement("td");
+    checker.appendChild(cb);
+    row.appendChild(checker);
+    for (let elements of valueList){
+        
+        let td = document.createElement("td");
+        td.innerHTML = elements;
+        if (elements=="Low"){
+            row.style.backgroundColor="green";
+        }
+        else if (elements=="Normal"){
+            row.style.backgroundColor="orange";
+        }
+        else if (elements=="Important"){
+            row.style.backgroundColor="yellow";
+        }
+        else if (elements=="Critical"){
+            row.style.backgroundColor="red";
+        }
+        row.appendChild(td);
+        
+    }
 
     parent.appendChild(row);
 }
 
-function removeRow() {
-    // https://stackoverflow.com/questions/26512386/remove-current-row-tr-when-checkbox-is-checked
-}
 
+function removeRow() {
+    {$("#taskList input[type='checkbox']:checked").closest("tr").remove();}
+}
 
 function populateSelect(selectId, sList) {
     let selections = document.getElementById(selectId)
@@ -48,16 +109,17 @@ function populateSelect(selectId, sList) {
         selections.appendChild(eachOption);
         }
     
-    }
+}
 
-//}
-//function populateSelect(selectId, sList) {
-    //let sel = document.getElementById(selectId, sList);
+function myFunction() {
+    var element = document.getElementById("#taskList");
+    element.classList.add("low");
+  }
 
-    // Implement
-//}
 
 window.onload = function() {
     populateSelect("assignedTo", team);
     populateSelect("priority", priority);
-};
+    document.querySelector("#title").value = ""
+    
+}
