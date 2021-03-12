6e8cda8236d7434d01ab4f8102f30c995bd4281d
Michael <musami01@luther.edu>
2021-03-11 22:36:05 -0600

    AnotherCommit

--- exercises/promises/js/script.js
+++ exercises/promises/js/script.js
@@ -3,13 +3,14 @@
 /* jshint node: true */
 'use strict';
 
-async function get_individual(num, all_numbers) {
-    let result = resolve_with_wait();
-    console.log(result);
-    console.log("Done");
+async function get_individual(num) {
+    return fetch('http://numbersapi.com/' + String(num))
+    .then (response => response.json())
+    .then (data => console.log(data))
+    .catch (error => console.log(error))
 }
 
-async function get_batch(num, all_numbers) {
+async function get_batch(num) {
     let result = await resolve_with_wait();
     console.log(result);
     console.log("Done");
@@ -19,8 +20,12 @@ async function clickedon() {
     let num = parseInt(document.querySelector('#number').value);
     let all_numbers = document.querySelector('#number_info');
     if (document.querySelector('#batch').checked) {
-        get_batch(num, all_numbers);
-    } else {
-        get_individual(num, all_numbers);
+    //    get_batch(num, all_numbers);
+    //} else {
+        get_individual(num);
     }
+}
+
+window.onload = function(){
+    clickedon();
 }
