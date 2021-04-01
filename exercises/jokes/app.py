6bb22f1120ccae979413d09ce531589d86dfcfe8
Michael <musami01@luther.edu>
2021-03-31 23:04:27 -0500

    AnotherCommit

--- exercises/jokes/app.py
+++ exercises/jokes/app.py
@@ -5,7 +5,7 @@ import random
 from typing import List
 
 import pyjokes
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -18,12 +18,20 @@ def index():
 
 @app.route("/", methods=["POST"])
 def index_jokes():
-    """Render the template with jokes"""
+    return render_template("/index.html")
     raise NotImplementedError
 
 
 def send_joke(
     language: str = "en", category: str = "all", number: int = 1
 ) -> List[str]:
+
+    req = request.get_json()
+
+    print(req)
+
+    res = make_response(jsonify({"message": "OK"}), 200)
+
+    return res
     """Return a list of jokes"""
     raise NotImplementedError
