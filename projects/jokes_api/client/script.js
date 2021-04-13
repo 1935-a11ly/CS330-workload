f23b8d911e07b9f08b8b5f66edadf301a3060ef4
Michael <musami01@luther.edu>
2021-04-12 23:31:16 -0500

    AnotherCommit

--- projects/jokes_api/client/script.js
+++ projects/jokes_api/client/script.js
@@ -1,3 +1,22 @@
 /* jshint esversion: 8 */
 'use strict';
+const BASE_URL =  "http://localhost:5000/api/v1/jokes"
+
+async function get_joke() {
+    language = document.getElementById(selLang);
+    numberOfJokes = document.getElementById(selNum);
+    jokeCategory = document.getElementById(selCat);
+    jokeId = document.getElementById(jokeId);
+    if (jokeId) {
+        return fetch(`${BASE_URL}/${language}/${numberOfJokes}/${jokeCategory}/${jokeId}`)
+        .then(response => response.json());
+    }
+    else{
+        return fetch(`${BASE_URL}/${language}/${numberOfJokes}/${jokeCategory}`)
+    .then(response => response.json());  
+    }
+}
+async function print_joke(){
+    
+}
 
--- projects/jokes_api/server/app.py
+++ projects/jokes_api/server/app.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
-"""
-jokes api
-"""
+
+#!/usr/bin/env python3
+"""Flask application to use pyjokes"""
+
+import random
+from typing import List
+
+import pyjokes
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/jokes/<language>/<category>/<int:number>")
+def index(number,language,category):
+
+    if  language == "es" and language == "gl" and category == "chuck" :
+        return "No kidding"
+    else:
+        alljokes = pyjokes.get_jokes(language=language, category=category)
+        dictionary={}
+        for i in range(len(alljokes)):
+            dictionary[i]=alljokes[i]
+        jokes = jsonify(dictionary)
+        jokes.headers["Access-Control-Allow-Origin"] = "*"
+        jokes.headers["Content-Type"] = "application/json"
+        return jokes
+
+@app.route("/api/v1/jokes/<language>/<category>/<int:number>/<int:id>")
+def index_jokes(number,language,category, id):
+    if  language == "es" and language == "gl" and category == "chuck" :
+        return "No kidding"
+    else:
+        alljokes = pyjokes.get_jokes(language=language, category=category)
+        dictionary={}
+        for i in range(len(alljokes)):
+            dictionary[i]=alljokes[i]   
+        jokes={id:dictionary[id]}
+        joke = jsonify(jokes)
+        return joke
+        
