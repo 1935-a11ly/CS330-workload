a47a1ff283b77d7a37e1b0ebfe2be6761bf23d16
Michael <musami01@luther.edu>
2021-04-13 22:13:57 -0500

    AnotherCommit

--- projects/jokes_api/server/app.py
+++ projects/jokes_api/server/app.py
@@ -23,15 +23,23 @@ def index(number,language,category):
         return error
     else:
         allFetchedJokes = pyjokes.get_jokes(language=language, category=category)
-        emptyDictionary={}
-        for completeJokeList in range(len(allFetchedJokes)):
-            emptyDictionary[completeJokeList]=allFetchedJokes[completeJokeList]
-        for individualJokeList in range(number):
-            jokes[individualJokeList]=emptyDictionary[individualJokeList]
-        joke = jsonify(jokes)
-        joke.headers["Access-Control-Allow-Origin"] = "*"
-        joke.headers["Content-Type"] = "application/json"
-        return joke
+        if number <= len(allFetchedJokes):
+            emptyDictionary={}
+            for completeJokeList in range(len(allFetchedJokes)):
+                emptyDictionary[completeJokeList]=allFetchedJokes[completeJokeList]
+            for individualJokeList in range(number):
+                jokes[individualJokeList]=emptyDictionary[individualJokeList]
+            joke = jsonify(jokes)
+            joke.headers["Access-Control-Allow-Origin"] = "*"
+            joke.headers["Content-Type"] = "application/json"
+            return joke
+        else:
+            jokes={"error 404":"Error 404, Joke not found"}
+            joke = jsonify(jokes)
+            joke.headers["Access-Control-Allow-Origin"] = "*"
+            joke.headers["Content-Type"] = "application/json"
+            return joke
+           
 
 @app.route("/api/v1/jokes/<language>/<category>/<int:number>/<int:id>")
 def index_jokes(number,language,category, id):
@@ -43,14 +51,21 @@ def index_jokes(number,language,category, id):
         return error
     else:
         allFetchedJokes = pyjokes.get_jokes(language=language, category=category)
-        emptyDictionary={}
-        for completeJokeList in range(len(allFetchedJokes)):
-            emptyDictionary[completeJokeList]=allFetchedJokes[completeJokeList]   
-        jokes={id:emptyDictionary[id]}
-        joke = jsonify(jokes)
-        joke.headers["Access-Control-Allow-Origin"] = "*"
-        joke.headers["Content-Type"] = "application/json"
-        return joke
+        if id >= len(allFetchedJokes):
+            errorMessage = {"error 404":"404 not found"}
+            error = jsonify(errorMessage)
+            error.headers["Access-Control-Allow-Origin"] = "*"
+            error.headers["Content-Type"] = "application/json"
+            return error
+        else:
+            emptyDictionary={}
+            for completeJokeList in range(len(allFetchedJokes)):
+                emptyDictionary[completeJokeList]=allFetchedJokes[completeJokeList]   
+            jokes={id:emptyDictionary[id]}
+            joke = jsonify(jokes)
+            joke.headers["Access-Control-Allow-Origin"] = "*"
+            joke.headers["Content-Type"] = "application/json"
+            return joke
 
 if __name__ == "__main__":
     app.run()
