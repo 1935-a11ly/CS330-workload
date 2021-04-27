c432dd305c1994a06819884ab205bbef880e5a63
Michael <musami01@luther.edu>
2021-04-27 11:57:23 -0500

    AnotherCommit

--- projects/fundraiser/app.py
+++ projects/fundraiser/app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 #!/usr/bin/env python3
-"""Flask application to use pyjokes"""
+"""Flask application for fundraiser"""
 
-import random
-from typing import List
 
-import pyjokes
-from flask import Flask, jsonify
+import requests
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
